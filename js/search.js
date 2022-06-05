3f48f8ec9481a04894c2493379dedacdc2161007
Fabian Kuffer <fabian.kuffer@kuffer.info>
2022-06-05 19:23:53 +0200

    stupid error

--- js/search.js
+++ js/search.js
@@ -186,7 +186,7 @@ function CreateTourArticle(image, name, difficulty, distance, duration, location
     button.classList.add("detail-button");
 
     button.addEventListener("click", function(){
-        window.location.href = "http://localhost:8080/tour.html?tourID="+tid;
+        window.location.href = "https://backend.cryptour.dullmer.de/tour.html?tourID="+tid;
     });
 
     p.innerHTML = "Dauer: " + duration + " Minuten<br>Länge: " + distance + " km<br>Schwierigkeit: " + difficulty;
